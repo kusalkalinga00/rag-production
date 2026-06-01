3ad7f0be61274e65bc9888a35f8e0521e13d5bee
Kusal Kalinga <kusalkalingainfo@gmail.com>
2026-06-01 14:11:04 +0530

    initial setup

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from lang-course!")
+from dotenv import load_dotenv
+from importlib.metadata import version
 
+load_dotenv()
+
+core_version = version("langchain-core")
+lg_version = version("langgraph")
+
+from langchain_openai import ChatOpenAI
+
+print(f"langchain-core version: {core_version}")
+print(f"langgraph version: {lg_version}")
+
+
+def main():
+    # Test OpenAI
+    
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    response = llm.invoke("What is the capital of France?")
+    print(f"OpenAI response: {response}")
+    
 
 if __name__ == "__main__":
     main()
